2c8bb6e0df487bb4450c00bda295bf904f005022
pallav100 <100pallav@gmail.com>
2024-01-22 21:10:44 -0800

    feat: add UI components

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Landing } from './componenets/pages/main';
+
+function App() {
+  return (
+    <div className="h-full w-full">
+        <Landing/>
+    </div>
+  );
+}
+
+export default App;
